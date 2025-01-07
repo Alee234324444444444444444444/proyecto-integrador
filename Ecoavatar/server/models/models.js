2a90c6df22c65734788b4c112efb4fb7a1e5acf3
Alee <apbarrionuevo@puce.edu.ec>
2025-01-06 21:54:07 -0500

    MODELOS

---
+++ Ecoavatar/server/models/models.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+// User Schema
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, maxlength: 75 },
+  user_name: { type: String, required: true, maxlength: 75 },
+  email: { type: String, required: true, unique: true },
+});
+
+// Post Schema
+const postSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  photo: { type: String, required: true },
+  description: { type: String, required: true, maxlength: 75 },
+  created_at: { type: Date, default: Date.now },
+});
+
+// Desafio Schema
+const desafioSchema = new mongoose.Schema({
+  titulo: { type: String, required: true, maxlength: 100 },
+  descripcion: { type: String, required: true },
+  tipo: { type: String, required: true, enum: ['diario', 'semanal'] },
+  completado: { type: Boolean, default: false },
+  usuario: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
+  fecha_limite: { type: Date, default: null },
+});
+
+// Recompensa Schema
+const recompensaSchema = new mongoose.Schema({
+  nombre: { type: String, required: true, maxlength: 100 },
+  imagen: { type: String, required: true },
+  desafio: { type: mongoose.Schema.Types.ObjectId, ref: 'Desafio', required: true },
+});
+
+// Personaje Schema
+const personajeSchema = new mongoose.Schema({
+  nombre: { type: String, required: true, maxlength: 100 },
+  usuario: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  recompensa: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recompensa', default: [] }],
+});
+
+module.exports = {
+  User: mongoose.model('User', userSchema),
+  Post: mongoose.model('Post', postSchema),
+  Desafio: mongoose.model('Desafio', desafioSchema),
+  Recompensa: mongoose.model('Recompensa', recompensaSchema),
+  Personaje: mongoose.model('Personaje', personajeSchema),
+};
--- Ecoavatar/server/server.js
+++ Ecoavatar/server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const path = require('path');
-
+let cors = require('cors')
 // Configuración de variables de entorno
 dotenv.config();
 
@@ -19,7 +19,14 @@ app.use(express.static(path.join(__dirname, '../client')));
 // Ruta principal
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html')); // Ruta correcta al archivo HTML
+    
 });
+
+// Conexión a MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/ecoavatar', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
 // Iniciar servidor
 app.listen(PORT, () => {
